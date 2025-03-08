c8e0bec5c1c410b02e953c4563a64e27d121687b
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-08 03:28:27 +0200

    implement search

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import  { useEffect, useState } from "react";
+import  { useState } from "react";
 import { fetchWeather } from "./api/weatherapi";
 import "./App.css";
 import { weatherprops } from "./types/weathertypes";
 
 function App() {
   const [weather, setWeather] = useState<weatherprops | null>(null);
+  const [city, setCity] = useState<string>("");
 
-  // type Weather = {
-  //   name: string;
-  //   main: {
-  //     temp: number | undefined;
-  //   };
-  //   weather: {
-  //     description: string;
-  //   }[];
-  // };
-
-  useEffect(() => {
-    const getweather = async () => {
-      try {
-        const data = await fetchWeather();
-        setWeather(data);
-      } catch (error) {
-        console.error("Error fetching weather data:", error);
-      }
-    };
-
-    getweather();
-  }, []);
+  const handleSearch = async () => {
+    try {
+      const data = await fetchWeather(city);
+      setWeather(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
 
   return (
     <>
+      <div>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       {weather && (
         <div>
           <h2>Weather in {weather.name}</h2>
--- src/api/weatherapi.ts
+++ src/api/weatherapi.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const keyy = "05aef76b4355cdab242f0489ab39d93f";
-const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=egypt&appid=${keyy}`;
 
-export const fetchWeather = async () => {
+export const fetchWeather = async (city: string) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${keyy}`;
   try {
     const response = await axios.get(url);
     return response.data;
